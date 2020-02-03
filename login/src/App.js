b040f804e4247e3526fdeda04a4b606270a56cf5
wildcard329 <enkiduwildman@gmail.com>
2020-02-03 09:53:03 -0800

    added registration page

--- login/src/App.js
+++ login/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import RegisterPage from './components/RegisterPage'
+import LoginPage from './components/LoginPage'
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path='/login'>
+        <LoginPage />
+      </Route>
+      <Route path='/register'>
+        <RegisterPage />
+      </Route>
     </div>
   );
 }
-
-export default App;
