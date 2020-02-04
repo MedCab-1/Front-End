368af5ad904ee18d293ab7feb39d90d6038c8fd6
wildcard329 <enkiduwildman@gmail.com>
2020-02-04 12:02:15 -0800

    uncommented header

--- login/src/App.js
+++ login/src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import LoginForm from './components/LoginForm'
 import { Route } from 'react-router-dom';
 import Footer from './components/Footer';
-// import Header from './components/header';
+import Header from './components/header';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-          {/* <Header /> */}
-        {/* <Route path='loginform' > */}
+          <Header />
+        <Route path='loginform' >
           <LoginForm />
-        {/* </Route> */}
+        </Route>
           <Footer />
     </div>
   );
