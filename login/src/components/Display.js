ff9d3491d8fb071074b2c9efab7c806b02129681
Devanee Reid <devaneereid@gmail.com>
2020-02-04 15:02:20 -0700

    created form to select strain types effects and flavors

---
+++ login/src/components/Display.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const Display = () => {
+    return (
+        <div>
+            <NavBar />
+        </div>
+    );
+};
+
+export default Display;
