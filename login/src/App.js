af3afc248f88055fe3d880c1205db77e6e94f318
wildcard329 <enkiduwildman@gmail.com>
2020-02-05 13:21:27 -0800

    fixed filepaths

--- login/src/App.js
+++ login/src/App.js
@@ -2,21 +2,19 @@ import React from 'react';
 import LoginForm from './components/LoginForm'
 import { Route } from 'react-router-dom';
 import Footer from './components/Footer';
-import Header from './components/header';
+import Header from './components/Header';
 import Signup from './components/Signup';
-
 
 export default function App() {
   return (
 
     <div className="App">
           <Header />
-        <Route path='loginform' >
+        <Route path='/LoginForm' >
           <LoginForm />
         </Route>
-        <Route path='signup' >
+        <Route path='/Signup' >
           <Signup />
-          <Route exact path='/' component={Signup} />
         </Route>
           <Footer />
     </div>
